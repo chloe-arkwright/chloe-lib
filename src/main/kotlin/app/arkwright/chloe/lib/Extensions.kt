6769782a864dc3de18cb3f884f98aab43756c192
Chloe Arkwright <chloe@arkwright.app>
2026-04-04 22:46:43 +0100

    Add BlockEntityRegistration.kt

--- src/main/kotlin/app/arkwright/chloe/lib/Extensions.kt
+++ src/main/kotlin/app/arkwright/chloe/lib/Extensions.kt
@@ -4,6 +4,8 @@ import net.minecraft.core.Registry
 import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.world.item.Item
 import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.block.entity.BlockEntityType
 
 fun <T : Item> T.register(): T {
 	return Registry.register(BuiltInRegistries.ITEM, this.builtInRegistryHolder().key(), this)
@@ -11,4 +13,8 @@ fun <T : Item> T.register(): T {
 
 fun <T : Block> T.register(): T {
 	return Registry.register(BuiltInRegistries.BLOCK, this.builtInRegistryHolder().key(), this)
+}
+
+fun <T : BlockEntity> BlockEntityType<T>.register(): BlockEntityType<T> {
+	return Registry.register(BuiltInRegistries.BLOCK_ENTITY_TYPE, this.builtInRegistryHolder().key(), this)
 }
---
+++ src/main/kotlin/app/arkwright/chloe/lib/registration/BlockEntityRegistration.kt
@@ -0,0 +1,19 @@
+package app.arkwright.chloe.lib.registration
+
+import net.minecraft.core.registries.Registries
+import net.minecraft.resources.ResourceKey
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.block.entity.BlockEntityType
+
+import net.fabricmc.fabric.api.`object`.builder.v1.block.entity.FabricBlockEntityTypeBuilder
+
+open class BlockEntityRegistration(modId: String) : Registration<BlockEntityType<*>>(modId, Registries.BLOCK_ENTITY_TYPE) {
+	fun <T : BlockEntity> type(key: ResourceKey<BlockEntityType<*>>, blocks: Array<Block>, factory: FabricBlockEntityTypeBuilder.Factory<T>): BlockEntityType<T> {
+		val type = FabricBlockEntityTypeBuilder.create(factory, *blocks).build()
+
+		type.builtInRegistryHolder().bindKey(key)
+
+		return type
+	}
+}
