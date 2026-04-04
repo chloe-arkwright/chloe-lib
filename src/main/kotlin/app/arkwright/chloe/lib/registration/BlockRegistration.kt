2f73a429bd181f194fba924c8b903af16fe0e0ef
Chloe Arkwright <chloe@arkwright.app>
2026-04-04 17:41:50 +0100

    Block and ItemRegistration need to be open.

--- src/main/kotlin/app/arkwright/chloe/lib/registration/BlockRegistration.kt
+++ src/main/kotlin/app/arkwright/chloe/lib/registration/BlockRegistration.kt
@@ -5,7 +5,7 @@ import net.minecraft.resources.ResourceKey
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.state.BlockBehaviour
 
-class BlockRegistration(modId: String) : Registration<Block>(modId, Registries.BLOCK) {
+open class BlockRegistration(modId: String) : Registration<Block>(modId, Registries.BLOCK) {
 	fun <T : Block> block(key: ResourceKey<Block>, createBlock: (BlockBehaviour.Properties) -> T, properties: BlockBehaviour.Properties): T {
 		val block = createBlock(properties.setId(key))
 
